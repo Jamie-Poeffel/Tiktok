839cf1bbc77ab45961eafda1351da8d91cdc68cb
Jamie Poeffel <Jamie_Poeffel@sluz.ch>
2024-11-11 21:49:26 +0100

    Navigation added and Routing added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+// src/components/Navbar.jsx
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </nav>
+  );
+}
